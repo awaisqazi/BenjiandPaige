974ef05e8d1aed03cffc21508aeac8a5c3ed6101
Awais Qazi <awais.a.qazi@gmail.com>
2026-04-14 12:23:51 -0500

    updating to mapouradventures.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://awaisqazi.github.io',
-  base: '/BenjiandPaige',
+  site: 'https://mapouradventures.com',
 });
